22120f917e1b9bf28d0d8cd54307aa6ca2102958
MartinCheong <martin.cej90@gmail.com>
2016-08-29 18:43:03 +0800

    adding html structure and title to static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
+  get '/home', controller: 'static_pages', action: 'home'
+  get '/help', controller: 'static_pages', action: 'help'
+  get '/about', controller: 'static_pages', action:'about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#hello'
