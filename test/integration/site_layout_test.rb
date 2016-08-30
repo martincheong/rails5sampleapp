3926a97af6cfd42623b18b7d60af9936aa29c025
MartinCheong <martin.cej90@gmail.com>
2016-08-30 14:58:26 +0800

    Finished layout and routes with tests

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -15,4 +15,10 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     get contact_path
     assert_select 'title', full_title("Contact")
   end
+
+  test "sign up page" do
+    get signup_path
+    assert_template 'users/new'
+    assert_equal full_title("Sign Up"), "Sign Up | Ruby on Rails Tutorial Sample App"
+  end
 end
