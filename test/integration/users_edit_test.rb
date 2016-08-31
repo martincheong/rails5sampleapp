6d4677d3408adaf939ba3d6c82c388e56c23755f
MartinCheong <martin.cej90@gmail.com>
2016-08-31 21:44:30 +0800

    added authorization for edit and update of profiles

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -3,9 +3,11 @@ require 'test_helper'
 class UsersEditTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:railstutorial)
+    @other_user = users(:archer)
   end
 
   test "unsuccesful edit" do
+    log_in_as(@user)
       get edit_user_path(@user)
       assert_template 'users/edit'
       patch user_path(@user), params:{user: {name:"",
@@ -17,6 +19,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "successful edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     name = "Foo bar"
@@ -31,4 +34,32 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_equal name,  @user.name
     assert_equal email, @user.email
   end
+
+  test "should redirect edit when not logged in" do
+   get edit_user_path(@user)
+   assert_not flash.empty?
+   assert_redirected_to login_url
+ end
+
+ test "should redirect update when not logged in" do
+   patch user_path(@user), params: { user: { name: @user.name,
+                                             email: @user.email } }
+   assert_not flash.empty?
+   assert_redirected_to login_url
+ end
+
+ test "should redirect edit when logged in as wrong user" do
+   log_in_as(@other_user)
+   get edit_user_path(@user)
+   assert flash.empty?
+   assert_redirected_to root_url
+ end
+
+ test "should redirect update when logged in as wrong user" do
+   log_in_as(@other_user)
+   patch user_path(@user), params:{user:{name: @user.name,
+                                         email: @user.email}}
+   assert flash.empty?
+   assert_redirected_to root_url
+ end
 end
