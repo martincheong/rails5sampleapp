1f006f518ba75e77ae51765fb2cd474cae5082c1
MartinCheong <martin.cej90@gmail.com>
2016-09-01 13:26:09 +0800

    added password reset and tests

--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,11 @@ Rails.application.routes.draw do
   get     '/login',     to: 'sessions#new'
   post    '/login',     to: 'sessions#create'
   delete  '/logout',    to: 'sessions#destroy'
+  
 
   resources :users, except: [:new]
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create,:edit,:update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   match '*path', to: redirect('/'), via: :all
 
