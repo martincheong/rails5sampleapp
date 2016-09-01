ebf262d38fc7a73d435534b4344d098c64942abb
MartinCheong <martin.cej90@gmail.com>
2016-09-01 12:08:30 +0800

    changed app url in production.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -54,6 +54,7 @@ class User < ApplicationRecord
   end
 
   private
+  
     def create_activation_digest
       self.activation_token  = User.new_token
       self.activation_digest = User.digest(activation_token)
